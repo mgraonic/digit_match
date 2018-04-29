db9a431e00652a8e9ac1355cc2d596de010f0f44
Maja Graonic <m.graonic@gmail.com>
2018-04-29 15:51:14 -0700

    all tests passing

--- lib/digit_match.rb
+++ lib/digit_match.rb
@@ -1,4 +1,14 @@
 # Returns count of digits matching in the two input non-negative integers
 def digit_match(number1, number2)
-  raise NotImplementedError
+
+  match = 0
+  return match if number1 == nil || number2 == nil
+  return match if number1 < 0 || number2 < 0
+
+  until number1 == 0 || number2 == 0
+    match += 1 if number1 % 10 == number2 % 10
+    number1 /= 10
+    number2 /= 10
+  end
+  return match
 end
